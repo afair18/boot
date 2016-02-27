d28c723ef867b07fcb4c5ed8cc79d5db9998327c
afair18 <afair18@naver.com>
2016-02-27 22:58:14 +0900

    hello 월드 삽입
    오픈함

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-eqiv="X-UA-compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scal=1">
+<title>bootstrap test</title>
+<link href="css/bootstrap.min.css" rel="sytlesheet">
+</head>
+<body>
+<h1>Hello, world!!</h1>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>

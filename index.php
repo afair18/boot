d18be8193e168deecd8963467065929c33a42eeb
afair18 <afair18@naver.com>
2016-11-30 21:32:14 +0900

    11추가
    누감표 넣어서요

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <link href="css/bootstrap.min.css" rel="sytlesheet">
 </head>
 <body>
-<h1>Hello, world!!</h1>
+<h1>Hello, world!!!!!!</h1>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 </body>
